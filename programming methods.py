6b838d01675f03dc80960770bd866c3da4a03da7
nAmkaH <112122620+nAmkaH@users.noreply.github.com>
2022-11-01 12:06:35 -0400

    Add files via upload

---
+++ programming methods.py
@@ -0,0 +1,7 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> np.random.randint(7, size=10)
+>>> np.random.randint(6, size=10)
+>>> print("The value of the arrays are: " + np)
+>>> print("The sum is: " + np)
